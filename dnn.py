69f148dcb2cf3bf29217c433e2ead728f1d3de76
Gustaf Holst <guho1700@student.miun.se>
2022-09-07 19:23:05 +0200

    fix y axis range of loss plot

--- dnn.py
+++ dnn.py
@@ -19,9 +19,10 @@ def build_and_compile_model(num_features):
 
 
 def plot_loss(history):
+    y_max = max(max(history.history['loss']), max(history.history['val_loss']))
     plt.plot(history.history['loss'], label='loss')
     plt.plot(history.history['val_loss'], label='val_loss')
-    plt.ylim([0, 1.5e6])
+    plt.ylim([0, y_max + y_max*0.1])
     plt.xlabel('Epoch')
     plt.ylabel('Error')
     plt.legend()
@@ -30,11 +31,7 @@ def plot_loss(history):
     plt.show()
 
 
-def train_model():
-    dataset = pd.read_csv('data/mel_spectrogram.csv')[:10000]
-
-    dataset.drop(['track_id', 'Unnamed: 0'], axis=1, inplace=True)
-
+def train_model(dataset):
     train_dataset = dataset.sample(frac=0.8, random_state=0)
     test_dataset = dataset.drop(train_dataset.index)
 
@@ -56,9 +53,7 @@ def train_model():
         train_labels,
         epochs=100,
         batch_size=BATCH_SIZE,
-        # Suppress logging.
         verbose=0,
-        # Calculate validation results on 20% of the training data.
         validation_split=0.2)
 
     plot_loss(history)
@@ -66,6 +61,9 @@ def train_model():
     hist = pd.DataFrame(history.history)
     hist['epoch'] = history.epoch
     print(hist.tail(20))
+
+    print(type(history.history['val_loss']))
+    print(history.history['val_loss'])
 
     test_results = {}
     test_results['model'] = model.evaluate(
@@ -75,4 +73,7 @@ def train_model():
 
 
 if __name__ == "__main__":
-    train_model()
+    df = pd.read_csv('data/mel_spectrogram.csv')[:10000]
+    df.drop(['track_id', 'Unnamed: 0'], axis=1, inplace=True)
+
+    train_model(df)
